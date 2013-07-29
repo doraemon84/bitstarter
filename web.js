f895059461ba5d1fef791867b77f7de8b9ea3652
dts <dts@ubuntu.(none)>
2013-07-29 14:23:15 -0700

    web.js update

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var test= fs.readFileSync('index.html') 
+
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(test);
 });
 
 var port = process.env.PORT || 5000;
